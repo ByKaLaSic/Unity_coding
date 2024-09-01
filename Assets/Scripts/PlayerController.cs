7e9e698a8ca55da24d622023ca27794cc7797fa6
Simakov Maksim <makszavr.1234554321@yandex.ru>
2024-09-01 05:10:07 +0300

    Added animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private bool _isPhysicalMovement = true;
-    [SerializeField] private float _speed = 10f;
-    
+    [SerializeField] private float _force = 0.8f;
+    [SerializeField] private float _moveSpeed = 3f;
+    [SerializeField] private float _rotateSpeed = 4f;
+    [SerializeField] private Animator _animator;
+
+    private bool _isWalking = false;
     private Rigidbody _rigidbody;
     private Vector3 _direction;
 
@@ -17,19 +21,27 @@ public class PlayerController : MonoBehaviour
     {
         _direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
 
-        if (_direction.magnitude >= 0.1f)
+        bool shouldWalk = _direction.magnitude >= 0.1f;
+
+        if (_isWalking != shouldWalk)
+        {
+            _isWalking = shouldWalk;
+            _animator.SetBool("isWalking", _isWalking);
+        }
+
+        if (_isWalking)
         {
             Quaternion targetRotation = Quaternion.LookRotation(_direction);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * _speed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * _rotateSpeed);
 
             if (!_isPhysicalMovement)
-                transform.Translate(_direction * _speed * Time.deltaTime, Space.World);
+                transform.Translate(_direction * _moveSpeed * Time.deltaTime, Space.World); 
         }
     }
 
     private void FixedUpdate()
     {
-        if (_isPhysicalMovement)
-            _rigidbody.velocity = _direction * _speed;
+        if (_isPhysicalMovement && _isWalking)
+            _rigidbody.AddForce(_direction * _force, ForceMode.VelocityChange);
     }
 }
